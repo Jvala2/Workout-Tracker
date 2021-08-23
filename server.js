37d8c7946d965983ec13b6d47436f1694fbe1052
Jvala2 <78758017+Jvala2@users.noreply.github.com>
2021-08-23 15:58:27 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://jvala2:TurnUpt03l3v3n11@cluster0.7pt2w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -18,7 +10,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
